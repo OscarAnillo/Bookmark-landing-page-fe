11c711600ef690d9e41e823afab07f5a0a0390a5
oscar anillo <oscarani14@gmail.com>
2022-12-14 19:25:31 -0500

    add mobileMenu with state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
+import { useState, useEffect } from "react";
+
+/* Components */
 import { NavComponent } from "./Components/nav-component";
 import { HeroComponent } from "./Components/hero-component";
 import { FeaturesComponents } from "./Components/features-components";
 import { ExtensionComponent } from "./Components/extension-component";
 import { FAQComponent } from "./Components/faq-component";
 import { ContactComponent } from "./Components/contact-component";
-
-import "./App.css";
 import { FooterComponent } from "./Components/footer-component";
 
+import "./App.css";
+
 function App() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const clickHandler = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [showMobileMenu]);
+
   return (
     <div className="App">
       <div className="container">
-        <NavComponent />
+        <NavComponent
+          showMobileMenu={showMobileMenu}
+          setShowMobileMenu={clickHandler}
+        />
         <HeroComponent />
         <FeaturesComponents />
         <ExtensionComponent />
--- src/Components/nav-component.js
+++ src/Components/nav-component.js
@@ -1,4 +1,4 @@
-export const NavComponent = () => {
+export const NavComponent = ({ showMobileMenu, setShowMobileMenu }) => {
   return (
     <nav className="nav">
       <div className="nav-flex">
@@ -14,9 +14,47 @@ export const NavComponent = () => {
           </ul>
         </div>
         <div className="mobile-menu">
-          <img src="images/icon-hamburger.svg" alt="" />
+          <img
+            src="images/icon-hamburger.svg"
+            alt=""
+            onClick={setShowMobileMenu}
+          />
         </div>
       </div>
+      {showMobileMenu && (
+        <div className="mobile-menu-bg">
+          <div className="mobile-menu-container">
+            <div className="mobile-menu-row">
+              <div>
+                <img
+                  src="images/logo-bookmark.svg"
+                  alt=""
+                  className="mobile-menu-logo"
+                />
+              </div>
+              <div>
+                <img
+                  src="images/icon-close.svg"
+                  alt=""
+                  onClick={setShowMobileMenu}
+                />
+              </div>
+            </div>
+            <div className="mobile-menu-items">
+              <ul>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Contact</li>
+                <li className="login-item">Login</li>
+              </ul>
+            </div>
+            <div className="mobile-menu-socials">
+              <img src="images/icon-facebook.svg" alt="" />
+              <img src="images/icon-twitter.svg" alt="" />
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
