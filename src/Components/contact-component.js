c3682094a2b58a1a266dfb21aa1fcf183de9688f
oscar anillo <oscarani14@gmail.com>
2022-12-14 21:26:21 -0500

    add email validation

--- src/Components/contact-component.js
+++ src/Components/contact-component.js
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const ContactComponent = () => {
-  const [emailInput, setEmailInput] = useState({
-    email: "",
-  });
+  const [emailInput, setEmailInput] = useState({ email: "" });
+  const [emailError, setEmailError] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
   const changeHandler = (e) => {
     const { name, value } = e.target;
     setEmailInput({ ...emailInput, [name]: value });
   };
+
+  const validate = (value) => {
+    const error = {};
+    if (!value.email) {
+      error.email = "An email is required!";
+    } else if (!emailRegex.test(value.email)) {
+      error.email = "Whoops, make sure it's an email";
+    }
+    return error;
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setEmailError(validate(emailInput));
+    setIsSubmitted(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(emailInput).length === 0 && isSubmitted) {
+      console.log(emailInput.email);
+    }
+  }, [emailError, emailInput, isSubmitted]);
 
   const { email } = emailInput;
   return (
@@ -16,14 +40,20 @@ export const ContactComponent = () => {
       <div className="contact-container">
         <p className="contact-paragraph">35,000+ already joined</p>
         <h2 className="contact-title">Stay up-to-date with what we're doing</h2>
-        <form>
+        <form onSubmit={submitHandler}>
           <input
             type="text"
-            placeholder="Enter your email address"
             name="email"
+            placeholder="Enter your email address"
             value={email}
             onChange={changeHandler}
           />
+          {emailError.email && (
+            <img src="images/icon-error.svg" alt="" className="img-error" />
+          )}
+          <p className={emailError.email ? "error-text" : "test-error"}>
+            {emailError.email}
+          </p>
           <button type="submit">Contact Us</button>
         </form>
       </div>
